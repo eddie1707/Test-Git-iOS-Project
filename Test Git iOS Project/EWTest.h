348f3a417922a7f06925a3f8a7abb2edf054963d
Edwin Wiersma <Edwin@MacBook-Pro-van-Edwin.local>
2014-05-20 13:19:05 +0200

    added a test to class

---
+++ Test Git iOS Project/EWTest.h
@@ -0,0 +1,13 @@
+//
+//  EWTest.h
+//  Test Git iOS Project
+//
+//  Created by Edwin Wiersma on 20-05-14.
+//  Copyright (c) 2014 Apps4Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EWTest : NSObject
+
+@end
---
+++ Test Git iOS Project/EWTest.m
@@ -0,0 +1,13 @@
+//
+//  EWTest.m
+//  Test Git iOS Project
+//
+//  Created by Edwin Wiersma on 20-05-14.
+//  Copyright (c) 2014 Apps4Mobile. All rights reserved.
+//
+
+#import "EWTest.h"
+
+@implementation EWTest
+
+@end
